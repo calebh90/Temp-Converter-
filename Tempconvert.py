57bcf716916b1154bec4d9cb521453fe8d43491a
calebh90 <calebh90@gmail.com>
2015-07-16 22:05:38 -0500

    Python 2.7 + attempt at class creation for i variable sake

--- Tempconvert.py
+++ Tempconvert.py
@@ -1,7 +1,6 @@
 """Temp converter"""
 
-initques = input("Hi! Type 'C' for C to F conversion or type 'F' for F to C conversion: ").lower()
-i = 0
+initques = raw_input("Hi! Type 'C' for C to F conversion or type 'F' for F to C conversion: ").lower()
 
 def functempc(x):
 	z = int(int(x)*1.8+32)
@@ -13,29 +12,31 @@ def functempf(x):
 	
 def mainfunc(x):
 	if x == "c":
-		Cinput = input("What temperature in Celsius do you want to convert to Fahrenheit? ")
+		Cinput = raw_input("What temperature in Celsius do you want to convert to Fahrenheit? ")
 		return(functempc(Cinput))
 	elif x == "f":
-		Finput = input("What temperature in Fahrenheit do you want to convert to Celsius? ")
+		Finput = raw_input("What temperature in Fahrenheit do you want to convert to Celsius? ")
 		return(functempf(Finput))
 	else:
 		return "Sorry, I don't understand. Please type 'C' for C to F conversion or 'F' for F to C conversion"
 	
 print(mainfunc(initques))
 
-restart = input("Type 'T' to convert another temperature or press Enter to quit: ").lower()
+restart = raw_input("Type 'T' to convert another temperature or press Enter to quit: ").lower()
+class restarting:
+	def counterfunc(x):
+		if x=="t":
+			i = 0
+			i = i + 1	
+			return i
+	print(counterfunc(restart))
+	def restartfunc(x):
+		if x == "t":
+			initques = raw_input("Type 'C' for C to F conversion or type 'F' for F to C conversion: ").lower()
+			return mainfunc(initques)
+		else:
+			return "Bye! "
 
-def counterfunc(x):
-	if x=="t":
-		i = i + 1
-	return counterfunc(restart)	
-def restartfunc(x):
-	if x == "t":
-		initques = input("Type 'C' for C to F conversion or type 'F' for F to C conversion: ").lower()
-		return mainfunc(initques)
-	else:
-		return "Bye! "
-
-while restart == "t" and i >= 1:
-	restart = input("Type 'T' to convert another temperature or press Enter to quit: ").lower()
-	print(restartfunc(restart))
+	while restart == "t" and i >= 1:
+		restart = raw_input("Type 'T' to convert another temperature or press Enter to quit: ").lower()
+		print(restartfunc(restart))
